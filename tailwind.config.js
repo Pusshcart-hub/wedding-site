9800c418a3afb3c967031966abb96abd0f255e4f
Pusshcart-hub <pusshcartgames@gmail.com>
2026-03-23 17:07:52 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+content: ["./index.html", "./src/**/*.{js,jsx}"],
+theme: {
+extend: {
+colors: {
+burgundy: "#6B1A2A",
+gold: "#C9A96E",
+cream: "#FAF5F0"
+}
+}
+},
+plugins: [],
+}
