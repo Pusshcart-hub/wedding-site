014a36b7c1dad452d174c0fa4576b8372d8134d3
Pusshcart-hub <pusshcartgames@gmail.com>
2026-03-23 17:34:32 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import Story from "./components/Story";
 import Timeline from "./components/Timeline";
 import RSVP from "./components/RSVP";
 import Nav from "./components/Nav";
+import Venue from "./components/Venue";
 
 export default function App() {
-return ( <div> <Hero /> <Story /> <Timeline /> <RSVP /> <Nav /> </div>
+return ( <div> <Hero /> <Story /> <Timeline /><Venue /> <RSVP /> <Nav /> </div>
 );
 }
