67b055fb610ee9efdff9801905414f56f4982227
Pusshcart-hub <pusshcartgames@gmail.com>
2026-03-23 19:01:03 +0800

    Update Intro.jsx

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -18,8 +18,14 @@ return ( <div className="fixed inset-0 z-50 bg-black text-white flex flex-col it
     </h1>
 
     <button
-      onClick={onEnter}
-      className="mt-6 px-6 py-3 border border-white rounded-full hover:bg-white hover:text-black transition duration-300"
+     onClick={() => {
+onEnter();
+const audio = new Audio("https://www.bensound.com/bensound-music/bensound-romantic.mp3");
+audio.loop = true;
+audio.play().catch(() => {});
+window.bgMusic = audio; // store globally
+}}
+className="mt-6 px-6 py-3 border border-white rounded-full hover hover transition duration-300"
     >
       Enter
     </button>
